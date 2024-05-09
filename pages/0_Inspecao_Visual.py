692de6ab5d7e791fab7a3e63659f46a548552060
YujiyMarks <43303495+YujiyMarks@users.noreply.github.com>
2024-05-09 14:56:01 -0300

    Update 0_Inspecao_Visual.py

--- pages/0_Inspecao_Visual.py
+++ pages/0_Inspecao_Visual.py
@@ -2,6 +2,7 @@
 import streamlit as st #biblioteca para a parte visual
 
 # bibliotecas para gerar o pdf
+from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, LongTable, TableStyle, Image, PageBreak
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
