307aedcd2779bc06b1000652ce2c18c76a0995ef
YujiyMarks <43303495+YujiyMarks@users.noreply.github.com>
2024-07-01 09:08:02 -0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -22,7 +22,7 @@ LOGGER = get_logger(__name__)
 
 
 def run():
-    st.set_page_config(page_title="Relatório",page_icon=":gear:",)
+    st.set_page_config(page_title="APP CS",page_icon=":gear:",)
     st.title("CS Consultoria")
     st.sidebar.success("HOME")
 
